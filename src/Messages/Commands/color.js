c85b187ac5664859bc687417ac28b7201aa54c2a
hollowlua <hollowc4d@gmail.com>
2024-07-21 23:22:54 -0300

    Update color.js

--- src/Messages/Commands/color.js
+++ src/Messages/Commands/color.js
@@ -4,7 +4,7 @@ const tick = require('../../Tick');
 
 const c_color = (c, input) => {
     if (input.trim() === "") { utils.msgClient(c, `Your current color is ${players.get(c.id).color}.`); return; }
-    if (input.trim().toUpperCase() === players.get(c.id).color) { utils.msgClient(c, `You already have this name!`); return; }
+    if (input.trim().toUpperCase() === players.get(c.id).color) { utils.msgClient(c, `You already have this color!`); return; }
     if (!utils.isColor(input.trim())) { utils.msgClient(c, `This color is invalid!`); return; }
 
     players.get(c.id).setColor(input.trim().toUpperCase());
